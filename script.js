02e47d95f121db9eb520720c1e8479126b1a19dc
Karl Vinuya <karl_vinuya@yahoo.ca>
2023-08-25 23:14:03 -0600

    Add function to draw on grid

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-function createSquareDivs(rows) {
+function createSquareDivs() {
     /*
         Purpose: Creates each box in the grid
-        Paremeter: x = amount of rows and columns
     */
 
     let div = document.createElement('div');
     div.className = 'grid';
-    let drawing_board = document.querySelector('#drawing-board');
-    let parent_width = drawing_board.offsetWidth;
+    let gridContainer = document.querySelector('#drawing-board');
 
-    // Get width for each gridbox
-    width = Math.floor(parent_width / rows) + 'px';
-    div.style.minWidth = width;
-
-    drawing_board.appendChild(div);
+    gridContainer.appendChild(div);
 }
 
 
@@ -22,9 +16,49 @@ function completeGrid(rows) {
         Purpose: Completes the whole grid
         Parameter: rows = amount of rows and columns
     */
+
+   let gridContainer = document.querySelector('#drawing-board');
+    gridContainer.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
     for (let i = 0; i < rows * rows; i++) {
-        createSquareDivs(rows);
+        createSquareDivs();
     }
 }
 
-completeGrid(50);
+function paintGrid(color) {
+    /*
+        Purpose: Paints the grid based on the desired color
+        Parameter: color: desired color
+    */
+
+    let drawingBoard = document.getElementById('drawing-board');
+    let isDragging = false;
+
+    // Initial click
+    drawingBoard.addEventListener('mousedown', event => {
+        if(event.target.className === 'grid') {
+            isDragging = true;
+            event.target.style.backgroundColor = color;
+
+        }
+    });
+
+    // Dragging click
+    drawingBoard.addEventListener('mousemove', event => {
+        if(isDragging) {
+            if(event.target.className === 'grid')
+                event.target.style.backgroundColor = color;
+        }
+    });
+
+    // User is not clicking
+    drawingBoard.addEventListener('mouseup', event => {
+        isDragging = false;
+    });
+}
+
+
+
+completeGrid(16);
+paintGrid('blue');
+
+
